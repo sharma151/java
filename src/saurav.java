9f62fde9ed5ae5d87245113daba6fb20ef296549
sharma151 <sharmasaurav150503@gmail.com>
2023-01-12 01:13:20 +0530

    substring added2

--- src/saurav.java
+++ src/saurav.java
@@ -2,5 +2,6 @@ public class saurav {
     public  static void main(String[] args){
         String name=" saurav sharma ";
         System.out.println( name.substring(0,7));
+        System.out.println(name.substring(8,15));
     }
 }
