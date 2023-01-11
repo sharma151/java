94edae50ba8905f1fb68bffde0811c5330089894
sharma151 <sharmasaurav150503@gmail.com>
2023-01-12 01:04:16 +0530

    initial change

--- src/helloworld.java
+++ src/helloworld.java
@@ -1,5 +1,8 @@
 public class helloworld {
     public static void main(String args[]){
         System.out.println("Hello Java");
+        String name =" saurav";
+        String name2 = name.replace('a','b');
+        System.out.println(name2);
     }
 }
