c51de0b516f734d912fcb59e2222cd6537a3c5d8
sharma151 <sharmasaurav150503@gmail.com>
2023-01-27 19:37:52 +0530

    substring added2

---
+++ src/Array.java
@@ -0,0 +1,10 @@
+public class Array {
+    public static void main(String[] args)
+    {
+        int[] Marks = new int[3];//declaration of Arrays.
+        Marks[0]=34;
+        Marks[1]=39;
+        Marks[2]=44;
+        System.out.println(Marks[2]);
+    }
+}
--- src/helloworld.java
+++ src/helloworld.java
@@ -2,7 +2,7 @@ public class helloworld {
     public static void main(String args[]){
         System.out.println("Hello Java");
         String name =" saurav";
-        String name2 = name.replace('a','b');
+        String name2 = name.replace('a','b');// replacig the character from the word.
         System.out.println(name2);
         System.out.println(name);
     }
