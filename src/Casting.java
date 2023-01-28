692cd9efcbf14c167fbc8481f631fc63f8ad246d
sharma151 <sharmasaurav150503@gmail.com>
2023-01-28 16:54:06 +0530

    casting added

---
+++ src/Casting.java
@@ -0,0 +1,12 @@
+public class Casting {
+    public static void  main(String[] args){
+        double price = 100.00;
+        double finalprice =price + 18;
+        System.out.println(finalprice);
+
+        int p =20;
+        int fp = p +(int)18.9;
+        System.out.println(fp);
+
+    }
+}
