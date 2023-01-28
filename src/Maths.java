11b57b3e0b4d42980b89189a28ba38d64bdfef44
sharma151 <sharmasaurav150503@gmail.com>
2023-01-28 17:03:00 +0530

    maths function added

---
+++ src/Maths.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class Maths {
+public static void main(String[] args){
+    System.out.println(Math.max(5,10));// To find maximum
+    System.out.println(Math.min(5,10));// To find minimum
+    System.out.println((int)(Math.random()*1000)); //To find the random number between 0-1000
+}
+}
