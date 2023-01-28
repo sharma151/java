33e0d6d30165adefc0c533278ddbb04e697f5048
sharma151 <sharmasaurav150503@gmail.com>
2023-01-28 22:04:45 +0530

    functions & operators added

---
+++ src/method_function.java
@@ -0,0 +1,9 @@
+public class method_function {
+    public static void sum(int a , int b){
+        int c = a +b;
+        System.out.println(c);
+    }
+    public static void main(String[] args){
+        sum(2,5);
+    }
+}
